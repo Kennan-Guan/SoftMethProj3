4fd71ab3da9e182abf6011416f1439e89a994a8b
Adwait <ganguly.adwait@gmail.com>
2022-11-05 23:38:57 -0400

    first commit with code

--- src/main/java/com/example/project3/GymManagerMain.java
+++ src/main/java/com/example/project3/GymManagerMain.java
@@ -12,7 +12,7 @@ public class GymManagerMain extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GymManagerMain.class.getResource("GymManagerView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 600);
-        stage.setTitle("Hello!");
+        stage.setTitle("Gym Manager Project 3");
         stage.setScene(scene);
         stage.show();
     }
